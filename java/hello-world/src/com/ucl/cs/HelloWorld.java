ce1c9f4ab0db30bf545a365f9addcc06ce9e3e7f
Fraser Savage <fraser_sav@yahoo.co.uk>
2015-09-20 17:46:43 +0100

    Added a java greeting to UCL CS peeps

---
+++ java/hello-world/src/com/ucl/cs/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.ucl.cs;
+
+import java.lang.System;
+
+/**
+ * @author Fraser
+ **/
+public class HelloWorld {
+
+    public void Main(String args[]) {
+        System.out.println("Hello UCL CS 2015-2018!");
+    }
+}
